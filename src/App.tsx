25f49a05edeee04693eaed4b09e25e3c96e8c8a4
Matheus Barbosa <matheusalolbarbosa@gmail.com>
2025-11-05 15:29:02 -0300

    feat(ui): create basic Login component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import Login from './components/Login';
+
+
 function App() {
   return (
     <div>
       <h1>Uptime Monitor</h1>
+      <Login />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Login.tsx
@@ -0,0 +1,58 @@
+import { useState } from 'react';
+
+
+interface LoginForm {
+  username: string;
+  password: string;
+}
+
+const Login = () => {
+  const [formData, setFormData] = useState<LoginForm>({
+    username: '',
+    password: '',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log('Form data to submit:', formData);
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={formData.username}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
