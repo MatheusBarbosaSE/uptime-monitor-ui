efa96e6db6d2a7fdf04d5a0c301b071d50185b00
Matheus Barbosa <matheusalolbarbosa@gmail.com>
2025-11-08 16:30:02 -0300

    feat(ui): install and configure Mantine UI framework

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+import '@mantine/core/styles.css';
+
+import { MantineProvider } from '@mantine/core';
+
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <MantineProvider defaultColorScheme="dark">
+      <App />
+    </MantineProvider>
+  </React.StrictMode>,
 )
